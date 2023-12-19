69aa4572045d5e13016c92351a365ca004c57473
woodwardtw <twwoodward@me.com>
2023-12-19 14:51:10 -0500

    homepage progress

--- functions.php
+++ functions.php
@@ -43,3 +43,44 @@ if ( class_exists( 'Jetpack' ) ) {
 foreach ( $understrap_includes as $file ) {
 	require_once get_theme_file_path( $understrap_inc_dir . $file );
 }
+
+
+function detox_homepage_posts(){
+	$args = array(
+		'post_type' => array('post'),
+		'post_status' => array('publish')
+
+	);
+	$the_query = new WP_Query( $args );
+
+	// The Loop
+	if ( $the_query->have_posts() ) :
+		while ( $the_query->have_posts() ) : $the_query->the_post();
+			global $post;
+			$title = get_the_title();
+			$link = get_the_permalink();
+			$img = get_the_post_thumbnail($post->ID,'medium', array('class' => 'img-fluid'));
+			$class = ($the_query->current_post % 2 == 0) ? 'col-md-7' : 'col-md-5' ;
+			$img_side = ($the_query->current_post % 3 == 0) ? 'order-md-last' : 'order-md-first';
+			$html = "<div class='{$class}'>
+					<div class='container-fluid'>
+						<div class='row'>
+							<div class='col-md-8 '>
+								<h2>{$title}</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem dui, tempor ac est vel, iaculis lacinia ipsum. Suspendisse magna lectus, cursus eget metus sit amet, sollicitudin porta purus. Donec eleifend magna mi, a sagittis ex sodales a. Nam convallis orci sapien, ac pretium ex pretium in. Pellentesque ullamcorper dapibus felis. Donec aliquet nibh sed dolor tincidunt fringilla. Etiam sit amet consectetur purus. Sed semper, dolor eu semper ornare, urna justo euismod velit, e</p>
+								<a class='btn btn-more' href='{$link}'>Explore</a>
+							</div>
+							<div class='col-md-4 {$img_side}'>
+								{$img}
+							</div>
+						</div>
+					</div>
+				</div>";
+		// Do Stuff
+		echo $html;
+		endwhile;
+	endif;
+
+	// Reset Post Data
+	wp_reset_postdata();
+}
--- loop-templates/content-home-page.php
+++ loop-templates/content-home-page.php
@@ -13,6 +13,7 @@ defined( 'ABSPATH' ) || exit;
 
 	<div class="entry-content contain-fluid">
 		<div class="row">
+			
 			<div class="left-long col-md-5">
 			<div class="internal-left-block yellow">
 			<h2>AI EVERYWHERE?</h2>
@@ -23,37 +24,8 @@ defined( 'ABSPATH' ) || exit;
 			<a href="#" class="btn btn-signup">Sign Up</a>
 			</div>
 		</div>
-		<div class="top-right col-md-7">
-			<div class="internal-top-block">
-			<div class="content">
-				<h2>It's not magic, it's math!</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem dui, tempor ac est vel, iaculis lacinia ipsum. Suspendisse magna lectus, cursus eget metus sit amet, sollicitudin porta purus. Donec eleifend magna mi, a sagittis ex sodales a. Nam convallis orci sapien, ac pretium ex pretium in. Pellentesque ullamcorper dapibus felis. Donec aliquet nibh sed dolor tincidunt fringilla. Etiam sit amet consectetur purus. Sed semper, dolor eu semper ornare, urna justo euismod velit, e</p>
-				<a class="btn btn-more" href="#">Explore</a>
-			</div>
-				<img src="http://experiments.middcreate.net/images/wp-content/uploads/sites/18/2023/12/comic.jpeg" width="400px" class="img-fluid">
-			</div>
-		</div>
-		<div class="center">
-			<div class="center-block">
-			<div class="content">
-			<h2>It's not friendship, it's programming</h2>
-			<p>Suspendisse magna lectus, cursus eget metus sit amet, sollicitudin porta purus. Donec eleifend magna mi, a sagittis ex sodales a. Nam convallis orci sapien, ac pretium ex pretium in. Pellentesque ullamcorper dapibus felis. Donec aliquet nibh sed dolor tincidunt fringilla. Etiam sit amet consectetur purus. Sed semper, dolor eu semper </p>
-			<a class="btn btn-more" href="#">Explore</a>
-			</div>     
-			<img src="http://experiments.middcreate.net/images/wp-content/uploads/sites/18/2023/12/robot.jpeg" class="img-fluid">
-			</div>     
-		</div>
-		<div class="bottom-left">
-			<div class="bl-block">
-			<img src="http://experiments.middcreate.net/images/wp-content/uploads/sites/18/2023/12/head_profile.png" class="img-fluid">
-			<div class="content">
-				<h2>It's not art, it's data</h2>
-				<p></p>
-			</div>
-			</div>
-		</div>
-		<div class="right-long"></div>
-		<div class="footer"></div>	
+		<?php detox_homepage_posts();?>
+		
 	</div>
 		<?php
 		the_content();
