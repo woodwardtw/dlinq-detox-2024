e4e3e2674426db09067e66eb4d11b6e62b8b6626
woodwardtw <twwoodward@me.com>
2024-01-02 16:05:07 -0500

    fix full width

--- page-templates/fullwidthpage.php
+++ page-templates/fullwidthpage.php
@@ -29,7 +29,7 @@ if ( is_page_template( 'page-templates/no-title.php' ) ) {
 
 		<div class="row">
 
-			<div class="col-md-8 offset-md-2 content-area" id="primary">
+			<div class="col-md-12 content-area" id="primary">
 
 				<main class="site-main" id="main" role="main">
 
