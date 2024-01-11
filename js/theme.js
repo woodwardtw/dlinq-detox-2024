402233564888acc1b9db3a50cfb9f0f0461a5441
woodwardtw <twwoodward@me.com>
2024-01-11 08:47:52 -0500

    ai conversation formatting
    js to detect and add classes
    
    fails with multi paragraph ai responses but I can probably fix those by hand

--- js/theme.js
+++ js/theme.js
@@ -6149,6 +6149,31 @@
     });
   });
 
+  //format AI conversation pastes
+  const botSection = document.querySelector('.entry-content');
+  const allParagraphs = botSection.querySelectorAll('p');
+  allParagraphs.forEach(p => {
+    const text = p.innerHTML;
+    const aiCheck = checkForSpeaker(text, 'AI: ');
+    const personCheck = checkForSpeaker(text, 'Human: ');
+    if (aiCheck === true) {
+      p.classList.add('bot');
+    }
+    if (personCheck === true) {
+      p.classList.add('human');
+    }
+    if (personCheck === true || aiCheck === true) {
+      p.classList.add('convo');
+    }
+  });
+  function checkForSpeaker(text, lookfor) {
+    const len = lookfor.length;
+    const subString = text.substring(0, len);
+    if (subString == lookfor) {
+      return true;
+    }
+  }
+
   exports.Alert = Alert;
   exports.Button = Button;
   exports.Carousel = Carousel;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -15,3 +15,30 @@ jQuery(document).ready(function() {
     });
 
 });
+
+
+//format AI conversation pastes
+const botSection =  document.querySelector('.entry-content');
+const allParagraphs = botSection.querySelectorAll('p');
+allParagraphs.forEach((p) => {
+ const text = p.innerHTML
+ const aiCheck = checkForSpeaker(text,'AI: ');
+ const personCheck = checkForSpeaker(text,'Human: ');
+  if(aiCheck === true){
+    p.classList.add('bot');
+  }
+  if(personCheck === true){
+    p.classList.add('human');
+  }
+  if(personCheck === true || aiCheck === true){
+    p.classList.add('convo')
+  }
+});
+
+function checkForSpeaker(text,lookfor){
+  const len = lookfor.length;
+  const subString = text.substring(0, len);
+  if(subString==lookfor){
+    return true;
+  }
+}
